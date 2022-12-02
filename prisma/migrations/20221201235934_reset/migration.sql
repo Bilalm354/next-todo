98f31da6f99729dad63a90d4860140e1689aebf0
bilalm354 <bilalm354@gmail.com>
2022-12-02 11:39:38 -0500

    removed prisma migrate files

--- prisma/migrations/20221201235934_reset/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "TodoItemData" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "text" TEXT NOT NULL,
-    "date" INTEGER NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "TodoItemData_id_key" ON "TodoItemData"("id");
